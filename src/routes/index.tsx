b2345292b4efe6f04a16a8fb016f71ac25d9ca2f
renterpoint <128383019+renterpoint@users.noreply.github.com>
2023-05-22 15:45:35 +0400

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,6 +8,9 @@ export default function Home() {
       <h1>Luxury Car Rental Dubai</h1>
       <Counter />
       <p>
+        If you want to take your driving experience to the next level then consider Exotic Car Rental Dubai services. With a wide range of high-performance vehicles available, you can choose the perfect car to suit your style and taste. Whether you're looking for a thrilling weekend getaway or a special occasion, an Exotic and Luxury Car Rental in Dubai is sure to provide an unforgettable experience.
+      </p>
+      <p>
         Visit{" "}
         <a href="https://start.solidjs.com" target="_blank">
           start.solidjs.com
